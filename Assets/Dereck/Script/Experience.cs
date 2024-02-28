31c17b1e0d107d78b27cb2cd28e5db2f8a597fcd
Linkdedo <zlink2499@gmail.com>
2024-02-27 20:03:27 -0500

    commencer faire Barre d'expérience sauvegader sur le github plusieurs prefab des menu et bar vie/expérience

---
+++ Assets/Dereck/Script/Experience.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Experience : MonoBehaviour
+{
+    public Image experienceBar;
+    public Text playerLevelTxt;
+    public int playerLevel = 1;
+    public float currentXP = 0;
+    public float maxXP;
+    public float rateXP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
