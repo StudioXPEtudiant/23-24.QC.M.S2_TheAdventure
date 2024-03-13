ccf858bea6185010dfefb80ebfa89ad0660ac817
Linkdedo <zlink2499@gmail.com>
2024-03-12 20:01:16 -0400

    continuer a faire la bar d'expérience presque fini

--- Assets/Dereck/Script/Experience.cs
+++ Assets/Dereck/Script/Experience.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Experience : MonoBehaviour
 {
     public Image experienceBar;
-    public Text playerLevelTxt;
+    public TMP_Text playerLevelTxt;
     public int playerLevel = 1;
     public float currentXP = 0;
-    public float maxXP;
-    public float rateXP;
+    public float maxXP = 100;
+    public float rateXP = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,21 @@ public class Experience : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (currentXP >= maxXP)
+        {
+            float reste = currentXP - maxXP;
+            playerLevel += 1;
+            playerLevelTxt.text = playerLevel;
+            currentXP = 0 + reste;
+            maxXP = maxXP * rateXP;
+        } 
+        experienceBar.fillAmount = currentXP / maxXP;
+    }
+
+    public void GainXP(float XP)
+    {
+        
+        currentXP += XP;
         
     }
 }
